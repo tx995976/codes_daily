3e14ae807ae771e87a6dc51120952b6770cb4337
tx995976 <tx995976@github.com>
2020-11-26 12:21:10 +0800

    tx995976

--- NOIp/test.cpp
+++ NOIp/test.cpp
@@ -8,6 +8,13 @@ void test(int a,int b){
 int test2(int a, int b){
     return 0;
 }
+int test3(int a, int b){
+    if(a != b){
+        return 0;
+    }
+    else return 1;
+
+}
 
 int main(){
     return 0;
