b168d3a4f11c386e76f863b1bc7daedf9192744a
tx995976 <tx995976@github.com>
2020-11-26 12:19:28 +0800

    tx995976

---
+++ NOIp/test.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+void test(int a,int b){
+    int a,b;
+    std::cin>>a>>b;
+    int c = a + b;
+}
+int test2(int a, int b){
+    return 0;
+}
+
+int main(){
+    return 0;
+}
---
+++ NOIp/test1.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int test(int a,int b){
+
+}
+
+int main(){
+    int k;                
+    while(cin>>k)
+        cout<<"continue";
+    cout<<"end";
+    getchar();
+    return 0;
+}
