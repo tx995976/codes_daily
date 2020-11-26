ac7b2e9de6ad3a04baf82bda0ba92ce0149f09c5
tx995976 <tx995976@github.com>
2020-11-26 18:08:52 +0800

    tx

--- NOIp/p5318.cpp
+++ NOIp/p5318.cpp
@@ -1,7 +1,29 @@
 #include<iostream>
 #include<vector>
+#include<queue>
 
 using namespace std;
 using itor_vec = vector<int>::const_iterator;
 
-vector<int> my_vec;
+
+queue<int> q_bfs;
+vector<int> d_vec;
+
+int dfs(){
+
+}
+
+int bfs(){
+
+}
+
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);
+    int n,m;
+    cin>>n>>m;
+    for(int i = 1;i <= m; i++){
+        
+    }
+
+
+}
