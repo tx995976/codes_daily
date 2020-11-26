d8438d2eef47ce2186e9dfefbdefb960cdb3750f
tx995976 <tx995976@github.com>
2020-11-27 00:03:39 +0800

    tx

--- NOIp/p5318.cpp
+++ NOIp/p5318.cpp
@@ -1,13 +1,23 @@
 #include<iostream>
 #include<vector>
 #include<queue>
+#include<set>
 
 using namespace std;
 using itor_vec = vector<int>::const_iterator;
 
+struct compare{
+    bool operator()(const int& a,const int& b)  const{
+        return a<b;
+    }
+};
+
+const int node = 1000010;
 
 queue<int> q_bfs;
-vector<int> d_vec;
+set<int> tree[node];
+bool searched[node];
+
 
 int dfs(){
 
@@ -19,11 +29,13 @@ int bfs(){
 
 int main(){
     ios::sync_with_stdio(0);cin.tie(0);
-    int n,m;
+    int n,m,a,b;
     cin>>n>>m;
     for(int i = 1;i <= m; i++){
-        
+        cin>>a>>b;
+        tree[a].insert(b);
     }
+    
 
 
 }
