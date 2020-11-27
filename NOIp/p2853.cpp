e621093b5ebfc6e1783310649b7b8c62762edd38
tx995976 <tx995976@github.com>
2020-11-27 19:08:38 +0800

    19.08

--- NOIp/p2853.cpp
+++ NOIp/p2853.cpp
@@ -1,19 +1,20 @@
 #include<iostream>
 #include<vector>
-#include<map>
-
+#include<cstring>
 using namespace std;
 
 const int node = 10100;
-map<int,int> tree_cow;
 vector<int> tree[node];
-int reached[node],cow[node];
+int cow[node],garden[1010];
 int k,n,m,a,b,total;
+bool reached[node];
 
 void cow_dfs(int id){
-    reached[id] += 1;
-    for(int i = 0;i < tree[id].size();i++)
-        cow_dfs(tree[id][i]);
+    reached[id] = 1;
+    garden[id] += 1;
+    for(int i = 0;i  < tree[id].size();i++)
+        if(!reached[tree[id][i]])
+            cow_dfs(tree[id][i]);
 }
 
 int main(){
@@ -25,6 +26,14 @@ int main(){
         cin>>a>>b;
         tree[a].push_back(b);
         }
-
+    for(int i = 1;i <= k;i++){
+        cow_dfs(cow[i]);
+        memset(reached,0,sizeof(reached));      
+    }
+    for(int i = 1;i <= k;i++){
+        if(garden[i] >= k)
+            total++;
+    }
+    cout<<total<<endl;
     return 0;
 }
