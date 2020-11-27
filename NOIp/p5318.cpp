adf996fa3c7f228e2e2c511c4aca064299b485cc
tx995976 <tx995976@github.com>
2020-11-27 14:47:14 +0800

    tx995976

--- NOIp/p5318.cpp
+++ NOIp/p5318.cpp
@@ -1,41 +1,54 @@
 #include<iostream>
 #include<vector>
 #include<queue>
-#include<set>
+#include<cstring>
+#include<algorithm>
 
 using namespace std;
-using itor_vec = vector<int>::const_iterator;
-
-struct compare{
-    bool operator()(const int& a,const int& b)  const{
-        return a<b;
-    }
-};
 
 const int node = 1000010;
+int n,m,a,b;
 
 queue<int> q_bfs;
-set<int> tree[node];
 bool searched[node];
+vector<int> tree[node];
 
-
-int dfs(){
-
+void dfs(int num,int item){
+    searched[num] = 1;
+    cout<<num<<" ";
+    if(item == n) return;
+    for(int k = 0;k < tree[num].size();k++)
+        if(!searched[tree[num][k]])
+            dfs(tree[num][k],item+1);
 }
 
-int bfs(){
-
+inline void bfs(int num){
+    memset(searched,0,sizeof(searched));
+    searched[num] = 1;
+    q_bfs.push(num);
+    while(!q_bfs.empty()){
+        int a = q_bfs.front();  q_bfs.pop();
+        cout<<a<<" ";
+        for(int k = 0;k < tree[a].size();k++)
+            if(!searched[tree[a][k]]){
+                q_bfs.push(tree[a][k]);
+                searched[tree[a][k]] = 1;
+            }
+    }
 }
 
 int main(){
     ios::sync_with_stdio(0);cin.tie(0);
-    int n,m,a,b;
     cin>>n>>m;
     for(int i = 1;i <= m; i++){
         cin>>a>>b;
-        tree[a].insert(b);
+        tree[a].push_back(b);
     }
-    
-
-
+    for(int i = 1;i <= n;i++)
+        sort(tree[i].begin(),tree[i].end());
+    dfs(1,0);   
+    cout<<endl;
+    bfs(1);
+    cout<<endl;
+    return 0;       
 }
