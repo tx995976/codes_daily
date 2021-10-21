04814f9748b1510ac4e1fe6710e3db9eafdb429e
tx995976 <tx995976@Gmail.com>
2021-10-21 14:39:41 +0800

    10.21(2)

--- oop/2_2/2/cal_Ls/Ls_stack.hpp
+++ oop/2_2/2/cal_Ls/Ls_stack.hpp
@@ -4,7 +4,7 @@
 template<class T>
 class Node{
     protected:
-        T *data;
+        T data;
         Node<T> *next;
     public:
         Node();
@@ -16,9 +16,7 @@ class Node{
 template<class T>
 class SLstack{
     protected:
-        int top;
         Node<T> *head;
-        Node<T> *get_Top;
     public:
         ~SLstack(){}
         SLstack();
@@ -32,11 +30,63 @@ class SLstack{
 
 template<class T>
 Node<T>::Node(){
-    this->data =nullptr;
-    this->next = nullptr;
-    
+    this->next = nullptr; 
 }
 
+template<class T>
+Node<T>::Node(const T &in){
+    this->data = in;
+    this->next = nullptr;
+}
+
+template<class T>
+Node<T>::Node(const Node<T> &in){
+    this->data = in.data;
+    this->next = in.next;
+}
+
+template<class T>
+SLstack<T>::SLstack(){
+    this->head = new Node<T>;
+}
+
+template<class T>
+int SLstack<T>::size(){
+    int i = 0;
+    auto temp = this->head->next;
+    while(temp->next != nullptr){
+        temp = temp->next;
+        i++;
+    }
+    return i;
+}
+template<class T>
+bool SLstack<T>::empty(){
+    if(this->head->next == nullptr)
+        return 1;
+    else
+        return 0;
+}
+template<class T>
+int SLstack<T>::pop(T &i){
+    auto temp = this->head->next;
+    auto temp2 = temp->next;
+    this->head->next = temp2;
+    i = temp->data;
+    delete temp;
+}
+template<class T>
+int SLstack<T>::push(T &i){
+    Node<T> * temp_in = new Node<T>(i);
+    auto temp = this->head->next;
+    this->head->next = temp_in;
+    temp_in->next = temp;
+}
+
+template<class T>
+T SLstack<T>::Top(){
+    return this->head->next->data;
+}
 
 
 
