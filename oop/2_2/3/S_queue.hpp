d4b5d9a5953b6af1b207fd1b5f337bda810ed537
tx995976 <tx995976@Gmail.com>
2021-10-31 16:41:53 +0800

    10.31(1)

--- oop/2_2/3/S_queue.hpp
+++ oop/2_2/3/S_queue.hpp
@@ -8,7 +8,7 @@ namespace my_queue{
     template <class T>
     class Sq_queue{
         protected:
-            int front,fear;
+            int top,fear;
             int max_size;
             T *data;
         public:
@@ -18,13 +18,50 @@ namespace my_queue{
             int size() const;
             bool empty() const;
             void clear();
-            int pop();
-            int push();
-            int front() const;
-            int back() const;
+            T pop();
+            int push(T &in);
+            T front() const;
+            T back() const;
     };
 
+    template<class T>
+    Sq_queue<T>::Sq_queue(){
+        this->top = 0;
+        this->fear = 0;
+        this->max_size = DEFAULT_SIZE;
+        data = new T[DEFAULT_SIZE];
+    }
     
+    template<class T>
+    int Sq_queue<T>::size()const{
+        return (this->fear-this->top+this->max_size)%this->max_size;
+    }
+    template<class T>
+    bool Sq_queue<T>::empty()const{
+        if(this->fear == this->top)
+            return 1;
+        else
+            return 0;
+    }
+    template<class T>
+    T Sq_queue<T>::front()const{
+        return this->data[this->top];
+    }
+    template<class T>
+    T Sq_queue<T>::back()const{
+        return this->data[(this->fear+this->max_size-1)%this->max_size];
+    }
+    template<class T>
+    T Sq_queue<T>::pop(){
+        T temp = this->front();
+        this->top = (this->top+1)%this->max_size;
+        return temp;
+    }
+    template<class T>
+    int Sq_queue<T>::push(T &in){
+        
+    }
+    template<class T>
 
 
 
