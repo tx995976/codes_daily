9cd1cdd5572849122bcc38caf725fe7a3d18e726
tx995976 <tx995976@Gmail.com>
2021-11-15 15:30:49 +0800

    oop_4_complete

--- oop/2_2/4/main.cpp
+++ oop/2_2/4/main.cpp
@@ -1,10 +1,37 @@
 #include "tree.hpp"
+#include <iostream>
 
 my_tree::Btree<std::string> name_tree;
 
-void menu(){
-    
+void tree_init(){
+    std::cout<<" ";
+    std::string str_input;
+    std::cin>>str_input;
+    name_tree.setting_up(str_input);
+}
 
+void menu(){
+    int i = 0;
+    while(1){
+        std::cout<<" "
+                <<" "
+                <<" ";
+        std::cin>>i;
+        switch(i){
+            case 1:
+                tree_init();
+                break;
+            case 2:
+                name_tree.post_order_re(name_tree.get_node());
+                break;
+            case 3:
+                name_tree.pre_order();
+                break;
+            default:
+                std::cout<<" ";
+                return;
+        }
+    }
 }
 
 int main(){
