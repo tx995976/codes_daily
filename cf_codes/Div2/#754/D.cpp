b4989aa8f28bc593e415b8852d7dddda5f1df743
tx995976 <tx995976@Gmail.com>
2021-11-16 20:13:05 +0800

    Div2#754_d_cp

--- cf_codes/Div2/#754/D.cpp
+++ cf_codes/Div2/#754/D.cpp
@@ -6,22 +6,20 @@ const int max_node = 2e5+1;
 
 std::vector<int> tree[max_node];
 bool find[max_node];
+bool ans[max_node];
 
 void find_node(int head_node,int this_node,int round,int line = 0){
     find[this_node] = 1;
     auto itor = tree[this_node].begin();
     for(;itor != tree[this_node].end();itor++){
-        if(find[*itor] == 0 && *itor^this_node <= (*itor < this_node ? *itor : this_node)){
+        if(find[*itor] == 0 && ((*itor^this_node) <= (*itor < this_node ? *itor : this_node))){
             find_node(head_node,*itor,round+1);
             line++;
         }
     }
     find[this_node] = 0;
-    if(line == 0){
-        if(round ^ 1 == 0){
-            
-        }
-    }
+    if(line == 0 && (round & 1) == 0)
+        ans[head_node] = 1;
     return;
 }
 void tree_build(){
@@ -35,9 +33,13 @@ void tree_build(){
         std::scanf("%d %d",&u,&v);
         tree[u].push_back(v);
         tree[v].push_back(u);
-    } 
+    }
     for(int i = 1;i<=j;i++){
+        ans[i] = 0;
         find_node(i,i,1);
+        if(ans[i] == 0){
+            std::printf("%d ",i);
+        }
     }
     std::printf("\n");
     while(j > 0){
