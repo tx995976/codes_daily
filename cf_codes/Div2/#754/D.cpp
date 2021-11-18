3471579f7a859a3270365b81ea9c69367422bede
tx995976 <tx995976@Gmail.com>
2021-11-18 12:04:22 +0800

    div2_d_ans?

--- cf_codes/Div2/#754/D.cpp
+++ cf_codes/Div2/#754/D.cpp
@@ -5,9 +5,10 @@
 const int max_node = 2e5+1;
 
 std::vector<int> tree[max_node];
-bool find[max_node];
-bool ans[max_node];
+std::vector<int>part[2];
+int bms[max_node];
 
+/*
 void find_node(int head_node,int this_node,int round,int line = 0){
     find[this_node] = 1;
     auto itor = tree[this_node].begin();
@@ -22,6 +23,28 @@ void find_node(int head_node,int this_node,int round,int line = 0){
         ans[head_node] = 1;
     return;
 }
+*/
+
+void bms_get(){
+     int pos = 1,cnt=1,next=2;
+     for(int i =1;i < max_node;i++){
+        bms[i] = pos;
+        if(--cnt == 0){
+            cnt = next;
+            next<<1;
+        }
+     }
+}
+
+void dfs_part(int this_node,int pre_node,int i){
+    part[i].push_back(this_node);
+        for(auto it :tree[this_node]){
+            if(it == pre_node)
+                continue;
+            dfs_part(it,this_node,i^1);
+        }
+}
+
 inline void tree_build(){
     int j =0,u,v;
     std::scanf("%d",&j);
@@ -34,7 +57,12 @@ inline void tree_build(){
         tree[u].push_back(v);
         tree[v].push_back(u);
     }
-    for(int i = 1;i<=j;i++){
+//////////////////////////////
+    bms_get();
+    dfs_part(1,1,0);
+
+
+  /*  for(int i = 1;i<=j;i++){
         ans[i] = 0;
         find_node(i,i,1);
         if(ans[i] == 0){
@@ -46,6 +74,7 @@ inline void tree_build(){
         tree[j].clear();
         j--;
     }
+    */
 }
 int main(){
     int i;
