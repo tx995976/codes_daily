46df7f522c0fff7aac0e7a7c183b2d70cb0b0655
tx995976 <tx995976@Gmail.com>
2021-11-23 20:20:11 +0800

    #whut_e

--- cf_codes/补题/#whut_2021/e.cpp
+++ cf_codes/补题/#whut_2021/e.cpp
@@ -6,14 +6,18 @@ int T,limit,a_n,b_x,b_y,a_z,a_i;
 int sit,z[max_n],z_i[max_n];
 
 int solve(){
-    int ans_max,ans_min,y = b_y;
+    int y_max = b_y,y_min = b_y;
     std::sort(z+1,z+a_n+1);
-    for(int i = 1;i <= a_n;i++){
+    for(int i = 1,j = a_n;i <= a_n;i++,j--){
         if(z[i] <= sit+1)
-            y -= (z[i]+1)*z_i[z[i]];
-        else{
-            
+            y_max -= (z[i]+1)*z_i[z[i]];
+        else if(z[i] <= sit+z_i[z[i]]){
+            y_max -= (z[i]+1)*z_i[z[i]]-((z[i]-sit+1)*(z[i]-sit+2)/2);
         }
+        else{
+            y_max -= 
+        }
+        sit -= z_i[z[i]];
     }
 }
 
@@ -22,8 +26,8 @@ int main(){
     while(T-- > 0){
         scanf("%d %d %d %d",&limit,&a_n,&b_x,&b_y);
         sit = limit;
-        scanf("%d %d",&a_z,&a_i);
         for(int i = 1;i <= a_n;i++){
+            scanf("%d %d",&a_z,&a_i);
             z[i] = a_z;
             z_i[a_z] = a_i;
             sit -= a_i;
