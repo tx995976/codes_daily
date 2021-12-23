109245349882bf2bf82d3a960e48bd53f396a293
tx995976 <tx995976@Gmail.com>
2021-12-23 22:00:32 +0800

    oop 7

--- oop/2_2/7/HashTable.hpp
+++ oop/2_2/7/HashTable.hpp
@@ -1,18 +1,31 @@
 #include<iostream>
 #include<algorithm>
 #include<cstdio>
+#include<cstring>
+#include<vector>
 
 namespace my_hashtable{
     const int MaxSize_Table = 5e5;
     const int MaxSize_word = 50;
     const int MaxSize_Strings = 10;
     const int MinSize_Strings = 3;
-    using hash_str = char[MaxSize_Strings];
+
+
+    //using hash_str = char[MaxSize_Strings];
+    //typedef char hash_str[MaxSize_Strings];
+
+    
+    
+    struct hash_str{
+        char str[MaxSize_Strings];
+    };
 
     //存储文件所有词语hash
     struct FileEntry{
-        int words;
+        int pos;
         FileEntry *next;
+        FileEntry(){}
+        FileEntry(int in){pos = in;next = nullptr;}
     };
 
 
@@ -20,6 +33,8 @@ namespace my_hashtable{
     struct WordEntry{ 
         int FileNo;
         WordEntry *next;
+        WordEntry(){}
+        WordEntry(int i){FileNo = i;next = nullptr;}
     };
 
     struct HashEntry{
@@ -34,7 +49,7 @@ namespace my_hashtable{
         private:
             int FileSize;
             FileEntry *files[MaxSize_word];
-            HashEntry *table[MaxSize_Table];
+            HashEntry *table;
 
         public:
             ~HashTable(){}
@@ -45,29 +60,104 @@ namespace my_hashtable{
             int Set_pos (int FileNo,int pos);
             double compare(int File1,int File2);
 
-            int InitTable_Words(hash_str strs[]);
+            int InitTable_Words(std::vector<hash_str> &strs,int File);
 
     };
 
-    int Hash(char *str){
+    int Hash(char str[]){
         unsigned hash = 0;
-        while(*str != '0'){
+        while(*str != '\000'){
             hash = (hash << 5) + (*str - 'a');
+            str++;
         }
         return hash % MaxSize_Table;
     }
+    
+    HashTable::HashTable(){
+        FileSize = 0;
+        table = new HashEntry[MaxSize_Table];
+    }
 
     int HashTable::Insert_pos(hash_str input){
-        return Hash(input);
+        return Hash(input.str);
     }
     
+
+    //不解决冲突
     int HashTable::Insert_table(hash_str input,int FileNo){
-
-
+        WordEntry *filelog;
+        int pos = this->Insert_pos(input);
+        if(table[pos].FileNO != FileNo){
+            if(!table[pos].FileNO){
+                //
+                strcpy(table[pos].str.str,input.str);
+            }
+            table[pos].FileNO = FileNo;
+            filelog = new WordEntry(FileNo);
+            filelog->next = table[pos].InvIndex;
+            table[pos].InvIndex = filelog;
+            //
+            Set_pos(FileNo,pos);
+            return pos;
+        }
+        return -1;
     }
 
+    int HashTable::Set_pos(int FileNo,int pos){
+        if(files[FileNo] == nullptr){
+            files[FileNo] = new FileEntry(pos);
+            return 1;
+        }
 
+        auto it = files[FileNo];
+        while(it->next != nullptr)
+            it = it->next;
+        it->next = new FileEntry(pos);
+        return 0;
+    }
 
+    double HashTable::compare(int File1,int File2){
+        double cnt = 0,all = 0;
+        FileEntry *f1,*f2;
+        f1 = files[File1];
+        f2 = files[File2];
+        //共有,一部分总体
+        while(f1 != nullptr){
+            auto it = table[f1->pos].InvIndex;
+            while(it != nullptr){
+                if(it->FileNo == File2){
+                    cnt += 1;
+                    break;
+                }
+                it = it->next;
+            }
+            all += 1;
+            f1 = f1->next;
+        }
+        //剩余总体词汇
+        while(f2 != nullptr){
+            auto it = table[f2->pos].InvIndex;
+            while(it != nullptr){
+                if(it->FileNo == File1)
+                    break;
+                it = it->next;
+            }
+            if(it == nullptr)
+                all += 1;
+            f2 = f2->next;
+        }
+        return (cnt/all);
+    }
 
- 
+    int HashTable::InitTable_Words(std::vector<hash_str> &strs,int File){
+        if(files[File] != nullptr){
+            return -1;
+        }
+        for(auto it : strs){
+            Insert_table(it,File);
+        }
+        this->FileSize++;
+        return 0;
+    }
+
 }
--- oop/2_2/7/main.cpp
+++ oop/2_2/7/main.cpp
@@ -1,44 +1,109 @@
 #include"HashTable.hpp"
 
 
+my_hashtable::HashTable word_table;
+std::vector<my_hashtable::hash_str> comple_str;
+std::vector<std::string> strs;
+
+int num_file = 1;
+
+void str_read(){
+    std::string temp_str;
+    my_hashtable::hash_str elem_temp;
+    int elem_len = 0;
+    bool full_flag = 0,overflow_flag=0;
+    char in = ' ';
+    fflush(stdin);
+    in = getchar();
+    while(in != '#'){
+        temp_str.push_back(in);
+        if(in >= 'a' && in <= 'z'){
+            if(full_flag){
+                overflow_flag = 1;
+            }
+            else{
+                elem_temp.str[elem_len++] = in;
+                if(elem_len == my_hashtable::MaxSize_Strings)
+                    full_flag = 1; 
+            }
+        }
+        else if(in >= 'A' && in <= 'Z'){
+            if(full_flag){
+                overflow_flag = 1;
+            }
+            else{
+                in = (in-'A')+'a';
+                elem_temp.str[elem_len++] = in;
+                if(elem_len == my_hashtable::MaxSize_Strings)
+                    full_flag = 1;
+            }
+        }
+        else{
+            if(!overflow_flag && elem_len >= my_hashtable::MinSize_Strings){
+                my_hashtable::hash_str elem;
+                strcpy(elem.str,elem_temp.str);
+                comple_str.push_back(elem);
+            }
+            memset(elem_temp.str,0,sizeof(my_hashtable::hash_str));
+            elem_len = 0,full_flag = 0,overflow_flag = 0;
+        }
+
+        in = getchar();
+        if(in == '\n'){
+            fflush(stdin);
+            in = ' ';
+        }
+    }
+
+    strs.push_back(temp_str);    
+    
+    return;
+}
+
+void new_file(){
+    printf("input(end flag: #):\n");
+    str_read();
+    word_table.InitTable_Words(comple_str,num_file);
+    comple_str.clear();
+    printf("complete\nFile ID:%d\n",num_file++);
+    return;
+}
+
+
+void compare_file(){
+    int f1,f2;
+    double result;
+    printf("input: id(file1,file2):\n");
+    scanf("%d %d",&f1,&f2);
+    result = word_table.compare(f1,f2);
+    printf("result:%.1f%\n");
+}
 
 
 void menu(){
     while(1){
-        printf("1.添加课程\n");
-        printf("2.添加修课顺序\n");
-        printf("3.课程数据改动\n");
-        printf("4.顺序数据改动\n");
-        printf("5.生成教学计划\n");
-        printf("0.退出\n");
+        printf("1.add files\n");
+        printf("2.cal_compare\n");
+        printf("0.exit\n");
         int i = 6;
         scanf("%d",&i);
         switch(i){
             case 1:
-                
+                new_file();
                 break;
             case 2:
-                
-                break;
-            case 3:
-                
-                break;
-            case 4:
-                
-                break;
-            case 5:
-                
+                compare_file();
                 break;
             case 0:
                 return;
             default:
-                printf("无效输入\n");
+                printf("invaild input\n");
                 break;
         }
     }
 }
 
 int main(){
-
+    menu();
     return 0;
 }
