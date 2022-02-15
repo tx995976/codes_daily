a85da8921d273e455c0dbc3bb6147bdb172a0516
tx995976 <tx995976@Gmail.com>
2022-02-15 12:24:56 +0800

    #t

--- cf_codes/Div2/#t_6/j.cpp
+++ cf_codes/Div2/#t_6/j.cpp
@@ -7,11 +7,11 @@
 
 using ll = long long;
 std::vector<ll> luc_num;
-int l,r;
+ll l,r;
 ll ans = 0;
 
-void table(int x){
-    if(x > 1e9)
+void table(ll x){
+    if(x > 1e10)
         return;
     luc_num.push_back(x);
     table(x*10+4);
@@ -19,20 +19,20 @@ void table(int x){
 }
 
 int main(){
-    scanf(" %d %d",&l,&r);
+    scanf(" %lld %lld",&l,&r);
     table(0);
     std::sort(luc_num.begin(),luc_num.end());
     int pos = 1;
-    while(l < r){
+    while(l <= r){
         while(l > luc_num[pos])
             pos++;
         if(r < luc_num[pos]){
-            ans += (ll)(r-l+1) * luc_num[pos];
-            l = r+1;
+            ans += ((r-l+1ll)*luc_num[pos]);
+            l = r+1ll;
         }
         else{
-            ans += (ll)(luc_num[pos]-l+1) * luc_num[pos];
-            l = luc_num[pos]+1;
+            ans += ((luc_num[pos]-l+1ll)*luc_num[pos]);
+            l = luc_num[pos]+1ll;
         }
     }
     printf("%lld\n",ans);
