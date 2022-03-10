1af71e2ecbab6c32b14fdddef873dba7cef5be9f
tx995976 <tx995976@Gmail.com>
2022-03-10 14:13:57 +0800

    #oop2-3

--- oop/2-3/2-b.cpp
+++ oop/2-3/2-b.cpp
@@ -9,6 +9,12 @@ std::list<int> q_list[2];
 int m,n;
 int id_list = 1;
 
+void prt(){
+    for(auto it : q_list[id_list^1])
+        printf("%4d",it);
+    printf("\n"); 
+}
+
 int main(){
     scanf(" %d",&m);
     for(int i = 1;i <= m;i++)
@@ -18,10 +24,7 @@ int main(){
         q_list[id_list^1].clear();
         auto it  = q_list[id_list].begin();
         while(!q_list[id_list].empty()){
-
-            int c = q_list[id_list].size();
-
-            int i = n;
+            int i = n-1;
             while(i--){
                 if(++it == q_list[id_list].end())
                     it = q_list[id_list].begin();
@@ -32,11 +35,8 @@ int main(){
 
             q_list[id_list^1].push_back(*er_it);
             q_list[id_list].erase(er_it);
-
-            
         }
     }
-    for(auto it : q_list[id_list^1])
-        printf("%-4d",it);
+    prt();
     return 0;
 }
