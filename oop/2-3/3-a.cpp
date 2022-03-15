cd49a9f786ebcd214f600f17c21117831a34c42e
tx995976 <tx995976@Gmail.com>
2022-03-15 16:16:12 +0800

    #oop2-3

--- oop/2-3/3-a.cpp
+++ oop/2-3/3-a.cpp
@@ -3,10 +3,33 @@
 #include<cstdio>
 #include<cmath>
 #include<deque>
+#include <vector>
 
+std::deque<int> stk;
+std::vector<int> nums,que;
+int n,ans = 0,temp;
 
 int main(){
-
-
+    scanf(" %d",&n);
+    for(int i = 1;i <= n;i++){
+        scanf(" %d",&temp);
+        nums.push_back(temp);
+    }
+    for(int i = 1;i <= n;i++){
+        scanf(" %d",&temp);
+        que.push_back(temp);
+    }
+    int pos_q = 0,size_q = que.size();
+    for(int pos_n = 0;pos_n < n && pos_q < size_q;){
+        while(!stk.empty()){
+            if(que.front() == que[pos_q]){
+                stk.pop_front();
+                pos_q++;
+                ans++;
+            }
+        }
+        stk.push_front(nums[pos_n++]);
+    }
+    printf("%d\n",ans == size_q ? ans : 0);
     return 0;
 }
