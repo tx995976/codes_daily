57df7d6752993ec4f6799955bbfe6bbe31cdaa48
tx995976 <tx995976@Gmail.com>
2022-03-23 15:28:51 +0800

    #abc+oop

--- oop/2-3/4-b.cpp
+++ oop/2-3/4-b.cpp
@@ -13,7 +13,8 @@ class tree{
     tree(){}
     ~tree(){
         delete value;
-        delete l_c,r_c;
+        delete l_c;
+        delete r_c;
     }
 };
 
@@ -23,19 +24,19 @@ std::string encode,ans;
 void build(std::string &enc,char temp){
     auto r = &root;
     for(auto it : enc){
-        auto next = it == '0' ? r->l_c : r->r_c;
-        if(next == nullptr)
-            next = new tree();
-        r = it == '0' ? r->l_c=next : r->r_c=next;
+        auto next = it == '0' ? &r->l_c : &r->r_c;
+        if(*next == nullptr)
+            *next = new tree();
+        r = *next;
     }
     r->value = new char(temp);
 }
 
 void get_cnt(tree *now){
-    if(now->r_c != nullptr)
-        get_cnt(now->r_c);
     if(now->l_c != nullptr)
         get_cnt(now->l_c);
+    if(now->r_c != nullptr)
+        get_cnt(now->r_c);
     printf("%d ",now->cnt);
 }
 
@@ -51,15 +52,14 @@ int main(){
 
     //freopen("in.txt","r",stdin);
     auto now = &root;
-    root.cnt++;
     while(~scanf(" %c",&temp)){
+        now->cnt++;
         now = temp == '0' ? now->l_c : now->r_c;
         if(now != nullptr && now->value != nullptr){
             ans.push_back(*now->value);
             now->cnt++;
             now = &root;
         }
-        now->cnt++;
     }
     printf("%s\n",ans.c_str());
     get_cnt(&root);
