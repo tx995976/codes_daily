e344f3f35e4cbec4e77e81a0f05d92283d562da2
tx995976 <tx995976@Gmail.com>
2022-05-26 10:08:23 +0800

    #t

--- contest_code/#whuut_3/l.cpp
+++ contest_code/#whuut_3/l.cpp
@@ -7,7 +7,7 @@
 
 const int max_n=1e6+10;
 int n,x,y;
-int pre[max_n];
+int pre[max_n],a[max_n];
 namespace alo{
     const int max_tree = max_n;
     int tree[max_tree];
@@ -37,10 +37,36 @@ namespace alo{
 int main(){
     scanf(" %d %d %d",&n,&x,&y);
     int tmp;
+	pre[0] = 1e5+10;
     for(int i = 1; i <= n; i++){
         scanf(" %d",&tmp);
-
+		if(tmp > x)
+			a[i] = 1;
+		else if(tmp < y)
+			a[i] = -1;
+		else
+			a[i] = 0;
+		pre[i] = a[i] + pre[i - 1];
     }
 
+	int ans = 0;
+	for(int i = 1; i <= n; i++){
+		if(pre[i] >= pre[0]){
+			ans += alo::query(pre[i]) + 1;
+		}
+		alo::add(pre[i], 1);
+	}
+
+	int emp = 0;
+	for(int i = 1; i <= n; i++){
+		if(a[i] == 0)
+			emp++;
+		else{
+			if(emp > 0)
+				ans -= (tmp + 1) * tmp / 2;
+			emp = 0;
+		}
+	}
+	printf("%d\n",ans);
     return 0;
 }
