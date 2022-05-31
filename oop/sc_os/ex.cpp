fcabb6c7d57f0598f55d081b74167632d890ce0c
tx995976 <tx995976@Gmail.com>
2022-05-31 16:23:35 +0800

    #os_ex

--- oop/sc_os/ex.cpp
+++ oop/sc_os/ex.cpp
@@ -3,8 +3,35 @@
 #include<cstring>
 #include<queue>
 
-int main(){
+const int max_process = 100;
+const int max_core = 4;
 
+const int wait = 1;
+const int run = 2;
+const int finish = 3;
+
+sturct Proc{
+	int pid;
+	std::string name;
+	int exec_time;
+	int need_time;
+	int start_time = -1;
+	int priority = 1;
+	int status = -1;
+};
+
+sturct Core_time{
+	Proc *proc;
+	int last_time;
+};
+
+Proc *proc_list[max_process];
+Core_time run_list[max_core];
+
+int time_now = 0;
+
+int main(){
+		
 
     return 0;
 }
